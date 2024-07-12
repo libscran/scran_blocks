57d90181b557a38b78891dd02700ecf7a84c97cd
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-07-12 16:08:37 -0700

    Added missing includes to the umbrella.

--- include/scran_blocks/scran_blocks.hpp
+++ include/scran_blocks/scran_blocks.hpp
@@ -1,6 +1,9 @@
 #ifndef SCRAN_BLOCKS_HPP
 #define SCRAN_BLOCKS_HPP
 
+#include "block_weights.hpp"
+#include "average_vectors.hpp"
+
 /**
  * @file scran_blocks.hpp
  * @brief Blocking utilities for **libscran**.
