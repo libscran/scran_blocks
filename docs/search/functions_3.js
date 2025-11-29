34f7dc93b2cdee1c64ae46275d5e44a67dab71a4
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-11-29 14:14:40 +0000

    Deploying to gh-pages from @ libscran/scran_blocks@36551a7d1efd4ddecd743d50f92ab5af8d64d21a 🚀

--- docs/search/functions_3.js
+++ docs/search/functions_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['singlequantile_0',['SingleQuantile',['../classscran__blocks_1_1SingleQuantile.html#a86b96118f4d7449d12d7b2899930c44e',1,'scran_blocks::SingleQuantile']]]
+  ['singlequantile_0',['SingleQuantile',['../classscran__blocks_1_1SingleQuantile.html#a37339757e92eb9cc2ba619ae77249542',1,'scran_blocks::SingleQuantile']]]
 ];
