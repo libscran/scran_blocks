761cc8d890b05abeb7f0ce46b79db594f8a01c4d
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-03-03 11:55:36 +1100

    Added quickstats dependency, removed redundant quantile classes.

--- include/scran_blocks/parallel_quantiles.hpp
+++ include/scran_blocks/parallel_quantiles.hpp
@@ -13,6 +13,7 @@
 #include "utils.hpp"
 
 #include "sanisizer/sanisizer.hpp"
+#include "quickstats/quickstats.hpp"
 
 /**
  * @file parallel_quantiles.hpp
@@ -20,140 +21,6 @@
  */
 
 namespace scran_blocks {
-
-/**
- * @brief Calculate a single quantile from a container.
- *
- * @tparam Output_ Floating-point type of the output quantile.
- * @tparam Iterator_ Random-access writeable iterator to a container.
- *
- * This class should be constructed once, given the number of elements and the desired probability.
- * It can then be used to compute a quantile for containers of size equal to the pre-specified number of elements. 
- * The quantile is type 7, consistent with the default in R's `quantile` function. 
- */
-template<typename Output_, class Iterator_>
-class SingleQuantile {
-public:
-    /**
-     * @param len Number of elements in the container, should be positive.
-     * @param quantile Quantile to compute, in \f$[0, 1]\f$.
-     */
-    SingleQuantile(const std::size_t len, const double quantile) {
-        assert(len > 0);
-        sanisizer::can_ptrdiff<Iterator_>(len);
-        const Output_ frac = static_cast<Output_>(len - 1) * static_cast<Output_>(quantile);
-        const Output_ base = std::floor(frac);
-        my_lower = base; // cast is known-safe if can_ptrdiff passes and 0 <= quantile <= 1.
-        my_upper_fraction = frac - base;
-        my_lower_fraction = static_cast<Output_>(1) - my_upper_fraction;
-        my_skip_upper = my_upper_fraction == 0;
-    }
-
-private:
-    std::size_t my_lower;
-    Output_ my_upper_fraction;
-    Output_ my_lower_fraction;
-    bool my_skip_upper;
-
-public:
-    /**
-     * @param begin Start of the container.
-     * @param end End of the container.
-     *
-     * @return Quantile for the sequence of elements in `[begin, end)`.
-     *
-     * The range `[begin, end)` should have length equal to `n`, and should not contain any NaN values.
-     * On output, the order of elements in `[begin, end)` may be rearranged.
-     */
-    Output_ operator()(Iterator_ begin, Iterator_ end) const {
-        assert(sanisizer::is_less_than(my_lower, end - begin));
-        auto target = begin + my_lower;
-        std::nth_element(begin, target, end);
-
-        // Avoid looking at target+1 if don't need it - in particular, we'd get a out-of-bounds access if quantile = 1.
-        if (my_skip_upper) {
-            return *target;
-        } else {
-            const auto next = std::min_element(target + 1, end); 
-            return static_cast<Output_>(*target) * my_lower_fraction + static_cast<Output_>(*next) * my_upper_fraction;
-        }
-    }
-};
-
-/**
- * @brief Calculate a single quantile for containers of variable length.
- *
- * @tparam Output_ Floating-point type of the output quantile.
- * @tparam Iterator_ Random-access writeable iterator to a container.
- *
- * This class should be constructed once, given the maximum number of elements and the desired probability.
- * It can then be used to compute quantiles from containers of size less than or equal to the maximum.
- * See `SingleQuantile` for more details.
- */
-template<typename Output_, class Iterator_>
-class SingleQuantileVariable {
-public:
-    /**
-     * @param max_len Maximum number of elements in the container.
-     * Unlike `SingleQuantile`, this may be zero.
-     * @param quantile Quantile to compute, in \f$[0, 1]\f$.
-     */
-    SingleQuantileVariable(const std::size_t max_len, const double quantile) : my_quantile(quantile) {
-        if (max_len >= 2) {
-            sanisizer::resize(my_choices, max_len - 1);
-        }
-    }
-
-private:
-    std::vector<std::optional<SingleQuantile<Output_, Iterator_> > > my_choices;
-    double my_quantile;
-
-public:
-    /**
-     * @param len Length of the container.
-     * This should be equal to `end - begin` and no greater than `max_len`.
-     * @param begin Start of the container.
-     * @param end End of the container.
-     *
-     * @return Quantile for the sequence of elements in `[begin, end)`.
-     * If this sequence is empty, NaN is returned.
-     *
-     * The range `[begin, end)` should have length equal to `len`, and should not contain any NaN values.
-     * On output, the order of elements in `[begin, end)` may be rearranged.
-     *
-     * This method is not thread-safe.
-     */
-    Output_ operator()(const std::size_t len, Iterator_ begin, Iterator_ end) {
-        assert(sanisizer::is_equal(len, end - begin));
-        if (len == 0) {
-            return std::numeric_limits<Output_>::quiet_NaN();
-        } else if (len == 1) {
-            return *begin;
-        } else {
-            auto& ocalc = my_choices[len - 2];
-            if (!ocalc.has_value()) { // Instantiating them on demand.
-                ocalc.emplace(len, my_quantile);
-            }
-            return (*ocalc)(begin, end);
-        }
-    }
-
-    /**
-     * @param begin Start of the container.
-     * @param end End of the container.
-     *
-     * @return Quantile for the sequence of elements in `[begin, end)`.
-     * If this sequence is empty, NaN is returned.
-     *
-     * The range `[begin, end)` should have length less than `max_length`, and should not contain any NaN values.
-     * On output, the order of elements in `[begin, end)` may be rearranged.
-     *
-     * This method is not thread-safe.
-     */
-    Output_ operator()(Iterator_ begin, Iterator_ end) {
-        return this->operator()(end - begin, begin, end);
-    }
-};
 
 /**
  * Compute the quantile for parallel elements across multiple arrays.
@@ -187,7 +54,7 @@ void parallel_quantiles(const std::size_t n, const std::vector<Stat_*>& in, cons
     tmp_buffer.reserve(nblocks);
 
     if (skip_nan) {
-        SingleQuantileVariable<Output_, I<decltype(tmp_buffer.begin())> > calcs(nblocks, quantile);
+        quickstats::SingleQuantileVariableNumber<Output_, I<decltype(nblocks)> > calcs(nblocks, quantile);
         for (std::size_t g = 0; g < n; ++g) {
             tmp_buffer.clear();
             for (I<decltype(nblocks)> b = 0; b < nblocks; ++b) {
@@ -196,18 +63,17 @@ void parallel_quantiles(const std::size_t n, const std::vector<Stat_*>& in, cons
                     tmp_buffer.push_back(val);
                 }
             }
-            out[g] = calcs(tmp_buffer.size(), tmp_buffer.begin(), tmp_buffer.end());
+            out[g] = calcs(tmp_buffer.size(), tmp_buffer.data());
         }
 
     } else {
-
-        SingleQuantile<Output_, I<decltype(tmp_buffer.begin())> > calc(nblocks, quantile);
+        quickstats::SingleQuantileFixedNumber<Output_, I<decltype(nblocks)> > calc(nblocks, quantile);
         for (std::size_t g = 0; g < n; ++g) {
             tmp_buffer.clear();
             for (I<decltype(nblocks)> b = 0; b < nblocks; ++b) {
                 tmp_buffer.push_back(in[b][g]);
             }
-            out[g] = calc(tmp_buffer.begin(), tmp_buffer.end());
+            out[g] = calc(tmp_buffer.data());
         }
     }
 }
--- tests/src/parallel_quantiles.cpp
+++ tests/src/parallel_quantiles.cpp
@@ -3,144 +3,6 @@
 #include "scran_tests/scran_tests.hpp"
 #include <vector>
 
-static int full_of_nans(const std::vector<double>& vec) {
-    int nan_count = 0;
-    for (auto x : vec) {
-        nan_count += std::isnan(x);
-    }
-    return nan_count;
-}
-
-TEST(SingleQuantile, Simple) {
-    std::vector<double> original { 0.7, 8.2, 2.3, 1.5, 8.8, 0.5 };
-    const std::size_t n = static_cast<std::size_t>(original.size());
-    scran_tests::CompareAlmostEqualParameters params;
-
-    // Make a copy to preserve the original unordered state, otherwise the vector ends up sorted and the test is too easy. 
-    {
-        auto values = original;
-        double val = scran_blocks::SingleQuantile<double, decltype(values.begin())>(n, 0)(values.begin(), values.end());
-        scran_tests::compare_almost_equal(val, 0.5, params);
-    }
-
-    {
-        auto values = original;
-        double val = scran_blocks::SingleQuantile<double, decltype(values.begin())>(n, 0.1)(values.begin(), values.end());
-        scran_tests::compare_almost_equal(val, 0.6, params);
-    }
-
-    {
-        auto values = original;
-        double val = scran_blocks::SingleQuantile<double, decltype(values.begin())>(n, 0.25)(values.begin(), values.end());
-        scran_tests::compare_almost_equal(val, 0.9, params);
-    }
-
-    {
-        auto values = original;
-        double val = scran_blocks::SingleQuantile<double, decltype(values.begin())>(n, 0.5)(values.begin(), values.end());
-        scran_tests::compare_almost_equal(val, 1.9, params);
-    }
-
-    {
-        auto values = original;
-        double val = scran_blocks::SingleQuantile<double, decltype(values.begin())>(n, 0.75)(values.begin(), values.end());
-        scran_tests::compare_almost_equal(val, 6.725, params);
-    }
-
-    {
-        auto values = original;
-        double val = scran_blocks::SingleQuantile<double, decltype(values.begin())>(n, 0.8)(values.begin(), values.end());
-        scran_tests::compare_almost_equal(val, 8.2, params);
-    }
-
-    {
-        auto values = original;
-        double val = scran_blocks::SingleQuantile<double, decltype(values.begin())>(n, 1)(values.begin(), values.end());
-        scran_tests::compare_almost_equal(val, 8.8, params);
-    }
-
-    // Works with some lower precisions too.
-    params.relative_tolerance = 1e-5;
-    {
-        auto values = original;
-        auto fval = scran_blocks::SingleQuantile<float, decltype(values.begin())>(n, 1)(values.begin(), values.end());
-        scran_tests::compare_almost_equal(static_cast<double>(fval), 8.8, params);
-    }
-
-    {
-        std::vector<float> fvalues(original.begin(), original.end());
-        auto val = scran_blocks::SingleQuantile<double, decltype(fvalues.begin())>(n, 1)(fvalues.begin(), fvalues.end());
-        scran_tests::compare_almost_equal(val, 8.8, params);
-    }
-}
-
-TEST(SingleQuantile, Complex) {
-    const std::size_t n = 54; // n - 1 should be prime so that we can use EXPECT_LT, GT, without worrying about float equality.
-    auto original = scran_tests::simulate_vector(n, {});
-
-    for (int val = 1; val < 100; ++val) {
-        const double prop = static_cast<double>(val)/100.0;
-        auto values = original;
-        const double Q = scran_blocks::SingleQuantile<double, decltype(values.begin())>(n, prop)(values.begin(), values.end());
-
-        double sum = 0;
-        for (auto x : original) {
-            sum += (x <= Q);
-        }
-
-        EXPECT_GT(prop, (sum-1)/(n-1));
-        EXPECT_LT(prop, sum/(n-1));
-    }
-
-    {
-        auto values = original;
-        const double Q = scran_blocks::SingleQuantile<double, decltype(values.begin())>(n, 0)(values.begin(), values.end());
-        EXPECT_EQ(Q, *std::min_element(original.begin(), original.end()));
-    }
-
-    {
-        auto values = original;
-        const double Q = scran_blocks::SingleQuantile<double, decltype(values.begin())>(n, 1)(values.begin(), values.end());
-        EXPECT_EQ(Q, *std::max_element(original.begin(), original.end()));
-    }
-}
-
-TEST(SingleQuantileVariable, Simple) {
-    std::vector<double> original { 0, 1, 2, 3, 4, 5 };
-    scran_blocks::SingleQuantileVariable<double, decltype(original.begin())> calc(original.size(), 0.5);
-
-    {
-        auto values = original;
-        auto Q = calc(values.begin(), values.begin());
-        EXPECT_TRUE(std::isnan(Q));
-    }
-
-    {
-        auto values = original;
-        auto Q = calc(values.begin(), values.begin() + 1);
-        EXPECT_EQ(Q, 0);
-    }
-
-    {
-        auto values = original;
-        auto Q = calc(values.begin(), values.begin() + 2);
-        scran_tests::compare_almost_equal(Q, 0.5, scran_tests::CompareAlmostEqualParameters());
-    }
-
-    {
-        auto values = original;
-        auto Q = calc(values.begin(), values.end());
-        scran_tests::compare_almost_equal(Q, 2.5, scran_tests::CompareAlmostEqualParameters());
-    }
-
-    // Re-using the cached calculator.
-    {
-        auto values = original;
-        auto Q = calc(values.begin() + 2, values.begin() + 4);
-        scran_tests::compare_almost_equal(Q, 2.5, scran_tests::CompareAlmostEqualParameters());
-    }
-}
-
 TEST(ParallelQuantiles, Simple) {
     std::vector<std::vector<double> > stuff {
         std::vector<double>{1, 2, 3, 4, 5},
@@ -160,7 +22,11 @@ TEST(ParallelQuantiles, Simple) {
     EXPECT_EQ(out_opt, stuff[0]);
 
     auto out_opt2 = scran_blocks::parallel_quantiles(5, std::vector<double*>{}, 0.5, false);
-    EXPECT_EQ(full_of_nans(out_opt2), out_opt2.size());
+    int nan_count = 0;
+    for (auto x : out_opt2) {
+        nan_count += std::isnan(x);
+    }
+    EXPECT_EQ(nan_count, out_opt2.size());
 }
 
 TEST(ParallelQuantiles, CheckNaNs) {
